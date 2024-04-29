807315ec2e90a7fbdc39a9f2789cafb44b1390b1
Orla Doyle <orla.doyle@novartis.com>
2024-04-29 06:13:28 -0700

    Pushed from Domino: https://nvs.domino-eval.com/workspace/doyleor1/cicd?executionId=662f90440af1800edae0d9bc

---
+++ prod/t_ae_severity.R
@@ -0,0 +1,57 @@
+
+#######################################################################################
+## Filename:                f_ae_count.R
+## Program Developer:       Orla Doyle
+
+## Project/Trial code:  AA1234/AA1234    
+## Description:  
+
+## Program history: 
+#######################################################################################
+
+
+####  ENV VARs
+
+DOMINO_RUN_ID <- Sys.getenv("DOMINO_RUN_ID")
+
+
+# paths
+data_path <- "/mnt/imported/data/SDTMBLIND/"
+output_path <- "/mnt/artifacts/report/saf/"
+hash_path <- "/mnt/code/.hashes/"
+
+# filenames
+pgm_stem <- "t_ae_"
+
+
+####  TFL GENERATION
+
+# libraries
+library(dplyr)
+library(stringr)
+library(haven)
+
+# read data
+ae <- haven::read_sas(file.path(data_path, "ae.sas7bdat"))
+
+
+# severity ----------------------------------------------------------------
+
+# count the number of patients by AE severity (AESEV variable)
+output_tag <- "severity"
+count_severity_by_patient <- ae %>%
+  dplyr::group_by(AESEV) %>%
+  dplyr::summarise(count_sev = n_distinct(AETERM))
+
+raw_artifact <- paste0(output_path, pgm_stem, output_tag, ".csv")
+write.csv(count_severity_by_patient,
+          raw_artifact)
+
+# get checksum of csv file
+cs_raw <- system(paste0("sha512sum ", raw_artifact, " | cut -d \" \" -f 1"), 
+                 intern=TRUE)
+write(cs_raw,
+      paste0(hash_path, pgm_stem, output_tag, '.txt'))
+
+#### end of file
+sessionInfo()
